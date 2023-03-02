684fbd3b328867c682d4aff0dabe8c1f437b2eb5
ivystephani <124930607+ivystephani@users.noreply.github.com>
2023-03-02 16:48:23 -0300

    Add files via upload

---
+++ 4-10.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+
+setlocale( LC_ALL, "portuguese");
+
+float numero;
+
+	cout << "Escolha um numero entre 1 e 7:" << endl <<"dia 01=segunda" << endl;
+	cin >> numero;
+
+if (numero <5) {
+	cout << "Dia útil";
+		
+} else if (numero= 6 or 7){
+	cout << "Fim de semana";
+		
+} 
+}
+
